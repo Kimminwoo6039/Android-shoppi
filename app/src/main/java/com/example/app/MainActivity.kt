1a341578865be8e5e871bc79aa1cde59cdc2252f
cas90 <qkqhemforhs1@gamail.com>
2022-10-09 20:38:22 +0900

    ..

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"ZZZZ")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"eeee")
     }
 }
