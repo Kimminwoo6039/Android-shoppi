605cc2fc1c78dd7a109f12b1feda2275b842bdc1
cas90 <qkqhemforhs1@gamail.com>
2022-10-27 19:56:24 +0900

    APi 30 이하
    API 31 이상
    
    SPlash 화면 이미지

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -7,19 +7,45 @@ import android.util.Log
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        Log.d(TAG,"onCreate")
+
     }
+
+    
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"onRestart")
+    }
+
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,"ZZZZ")
+        Log.d(TAG,"onStart")
     }
 
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG,"eeee")
+        Log.d(TAG,"onResuum")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onstop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy")
     }
 }
