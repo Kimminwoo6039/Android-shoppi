404b190957ad203697b3608caa666bb7ddce8f04
cas90 <qkqhemforhs1@gamail.com>
2022-10-28 22:49:37 +0900

    test 커밋

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
-        Log.d(TAG,"onCreate12")
+        Log.d(TAG,"onCreate")
 
 
     }
